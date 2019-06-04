032664de8a1fa07dd0520b05992f182f0ec19988
Amdm10 <michalska.ana@gmail.com>
2019-06-04 23:50:13 +0200

    StreamForumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExecuteSaySomething;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
@@ -11,11 +12,8 @@ public class StreamMain {
     public static void main(String[] args) {
         System.out.println("Welcome to module 7 - Stream");
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("love", txt -> "ABC" + txt + "ABC");
-        poemBeautifier.beautify("love", txt -> txt.toUpperCase());
-        poemBeautifier.beautify("love", txt -> "*" + txt.substring(0,1) + "*" + txt.substring(1,2) + "*" + txt.substring(2,3) + "*" + txt.substring(3,4) + "*");
-        poemBeautifier.beautify("love",txt -> txt.substring(0,1).toUpperCase()+txt.substring(1,2)+txt.substring(2,3).toUpperCase()+txt.substring(3,4));
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
     }
 }
