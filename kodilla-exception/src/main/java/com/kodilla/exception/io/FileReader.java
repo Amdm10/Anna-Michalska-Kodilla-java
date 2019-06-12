faa517e25bb1ca3d5256048451cd709c2ab5dcdb
Amdm10 <michalska.ana@gmail.com>
2019-06-12 22:36:09 +0200

    FirstChallenge

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -13,17 +13,19 @@ public class FileReader {
 
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("names.txt").getFile());
-        Path path = Paths.get(file.getPath());
 
-        try {
 
-            Stream<String> fileLines = Files.lines(path);
+        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
 
             fileLines.forEach(System.out::println);
 
         } catch (IOException e) {
-            System.out.println(e);
-            System.out.println("Oh no! Something went wrong!");
+
+           System.out.println("Oh no! Something went wrong! Error:" + e);
+
+        } finally {
+
+            System.out.println("I am gonna be here... always!");
         }
 
     }
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,32 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if(b == 0) {
+            throw  new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        try {
+
+            double result = firstChallenge.divide(3,0);
+            System.out.println(result);
+
+        } catch (ArithmeticException a) {
+
+            System.out.println("You are trying to divide by zero. Error: " + a);
+
+        } finally {
+
+            System.out.println("The end");
+        }
+
+
+    }
+}
